938b11ffb68493f2f2e57a41c8bf9bc45e47093a
Eugenia Kang <enarikang@gmail.com>
2015-10-22 03:27:13 -0700

    Updated json + pagerank files

--- pagerank.py
+++ pagerank.py
@@ -32,6 +32,9 @@ class Worker(threading.Thread):
         self.last10 = last10
         self.N = N
         self.thread_num = thread_num
+        self.pagesdict = defaultdict(int)
+        self.all_links = set([url])
+
 
     def run(self):
         """
@@ -43,7 +46,7 @@ class Worker(threading.Thread):
             if random.random() < 0.95 and len(set(self.last10)) > 4:
                 try:
                     # list of urls with words in stopwords filtered out
-                    soup, url_list = parse_links(current_url, url_start)
+                    soup, url_list = self.parse_links(current_url, self.url)
                     url_list = filter(lambda link: sum([word in link for word in stopwords]) == 0, url_list)
                     current_url = random.choice(url_list)
                     current_url_title = soup.title.text.strip()[:-14]  # removes MayoClinic
@@ -53,117 +56,37 @@ class Worker(threading.Thread):
                     # count href only if it is a symptom
                     for a in set(hrefs):
                         if '/symptoms/' in a["href"]:
-                            pagesdict[a.text.lower()] += 1
+                            self.pagesdict[a.text.lower()] += 1
                 except:
-                    current_url = random.sample(all_links, 1)[0]
+                    current_url = random.sample(self.all_links, 1)[0]
             else:
                 # click the "home" button!
-                current_url = random.sample(all_links, 1)[0]
+                current_url = random.sample(self.all_links, 1)[0]
 
             self.last10.popleft()
             self.last10.append(current_url)
-            all_links.add(current_url)
+            self.all_links.add(current_url)
 
-
-def parse_links(url, url_start):
-    url_list = []
-    # open, read, and parse the text using beautiful soup
-    try:
-        page = urllib.urlopen(url)
-        text = page.read()
-        page.close()
-        soup = BeautifulSoup(text, "lxml")
-        # find all hyperlinks using beautiful soup
-        hrefs = soup.find("div", {"class": "contentbox links show-more"}).findAll("a", href=True)
-        for tag in set(hrefs):
-            # concatenate the base url with the path from the hyperlink
-            if tag['href'].startswith('/symptoms/') or tag['href'].startswith('/diseases-conditions/'):
-                tmp = urlparse.urljoin(url, tag['href'])
-                url_list.append(tmp)
-        if len(url_list) == 0:
-            return soup, [url_start]
-        return soup, url_list
-    except:
-        return None, [url_start]
-
-
-def initialize_rank(diseases, symptoms):
-    """ Initialize importance of pages.
-
-    All pages should have an initial value of
-    1/(number of pages).
-
-    Keyword arguments:
-    diseases  -- dictionary of diseases
-    symptoms  -- dictionary of symptoms
-    """
-    num_pages = len(diseases) + len(symptoms)
-    for key in diseases:
-        diseases[key] = 1/num_pages
-    for key in symptoms:
-        symptoms[key] = 1/num_pages
-    # Return initialized ranks for diseases and symptoms
-    return diseases, symptoms
-
-
-def assign_page_rank(pagesdict, ranked_diseases, ranked_symptoms):
-    """ Assign a page rank to each disease and symptom.
-    (See http://infolab.stanford.edu/~taherh/papers/topic-sensitive-pagerank-tkde.pdf)
-    Weightings should be different depending on what is
-    referencing what.
-
-    The more diseases that link to a disease, the less likely
-    a symptom that is referenced by that disease will be indicative
-    of the disease itself (the symptom could be a manifestation of some
-    other related disease).
-
-    The more symptoms that link to a disease, the less likely that any
-    symptom referenced by that disease is a guarantee of the disease itself.
-
-    The more diseases that link to a symptom, the less likely that having
-    the symptom is indicative of any one disease.
-
-    The more symptoms that link to a symptom, the less likely that the
-    symptom by itself will provide much information about any given disease.
-
-    Keyword arguments:
-    pagesdict       -- a complete dictionary of all the pages
-    ranked_diseases -- initialized rank dictionary of diseases
-    ranked_symptoms -- initialized rank dictionary of symptoms
-    """
-    # Let first value of dict[key] be the list of links that point to the key.
-    # Let second value of dict[key] be the number of outgoing links from that key.
-    threshold = 4/len(pagesdict)
-    changing = True
-    while changing:
-        # If fewer than 5 pages significantly change rank, then we can
-        # say PageRank is finished.
-        num_over_threshold = 0
-        for key in pagesdict:
-            new_rank = 0
-            for link in pagesdict[key][0]:
-                if link in ranked_diseases:
-                    # Link was from a disease.
-                    new_rank += 2*ranked_diseases[link]/pagesdict[link][1]
-                else:
-                    # Link was from a symptom.
-                    new_rank += ranked_symptoms[link]/pagesdict[link][1]
-            if key in ranked_diseases:
-                # Looking at a disease. Check threshold and
-                # assign the new rank.
-                if abs(ranked_diseases[key] - new_rank) > threshold:
-                    num_over_threshold += 1
-                ranked_diseases[key] = new_rank
-            else:
-                # Looking at a symptom. Check threshold and
-                # assign the new rank.
-                if abs(ranked_symptoms[key] - new_rank) > threshold:
-                    num_over_threshold += 1
-                ranked_symptoms[key] = new_rank
-        if num_over_threshold < 5:
-            # Page rank is finished.
-            changing = False
-
+    def parse_links(self, url, url_start):
+        url_list = []
+        # open, read, and parse the text using beautiful soup
+        try:
+            page = urllib.urlopen(url)
+            text = page.read()
+            page.close()
+            soup = BeautifulSoup(text, "lxml")
+            # find all hyperlinks using beautiful soup
+            hrefs = soup.find("div", {"class": "contentbox links show-more"}).findAll("a", href=True)
+            for tag in set(hrefs):
+                # concatenate the base url with the path from the hyperlink
+                if tag['href'].startswith('/symptoms/') or tag['href'].startswith('/diseases-conditions/'):
+                    tmp = urlparse.urljoin(url, tag['href'])
+                    url_list.append(tmp)
+            if len(url_list) == 0:
+                return soup, [url_start]
+            return soup, url_list
+        except:
+            return None, [url_start]
 
 num_of_visits = 12 if len(sys.argv) < 2 else int(sys.argv[1])
 stopwords = ['video', 'media', '#']
@@ -176,16 +99,11 @@ if len(sys.argv) > 2:
     for key, val in jsondict.items():
         pagesdict[key] = val
 
-# ~~~~~~~~~DO MORE WITH THESE ~~~~~~~~~~~
-# ranked_diseases = defaultdict()
-# ranked_symptoms = defaultdict()
-# num_pages = len(pagesdict)
 url_start1 = "http://www.mayoclinic.org/symptoms/cough/basics/definition/sym-20050846"
 url_start2 = "http://www.mayoclinic.org/symptoms/nausea/basics/definition/sym-20050736"
 url_start3 = "http://www.mayoclinic.org/diseases-conditions/carbon-monoxide/basics/definition/con-20025444"
 url_start4 = "http://www.mayoclinic.org/diseases-conditions/heart-attack/basics/definition/con-20019520"
 start_links = [url_start1, url_start2, url_start3, url_start4]
-all_links = set(start_links)
 workers = []
 t = time.time()
 
@@ -195,6 +113,8 @@ for i, url_start in enumerate(start_links):
     workers.append(worker)
 
 for worker in workers:
+    for key in worker.pagesdict:
+        pagesdict[key] += worker.pagesdict[key]
     worker.join()
 
 for x in pagesdict.items():
--- pagerank2.py
+++ pagerank2.py
@@ -12,12 +12,6 @@ import urlparse
 from bs4 import BeautifulSoup
 from collections import defaultdict, deque
 
-# Sort disease_conditions by current pagerank value
-# ranked_diseases = sorted(diseases_conditions.items(), key=itemgetter(1))
-
-# Rank symptoms by order of significance
-# sorted(symptoms_dict, key=itemgetter(1), reverse=True)
-
 
 class Worker(threading.Thread):
 
@@ -33,6 +27,11 @@ class Worker(threading.Thread):
         self.last10 = last10
         self.N = N
         self.thread_num = thread_num
+        self.pagesdict = defaultdict(lambda: [[], 1])
+        self.diseases = defaultdict(int)
+        self.symptoms = defaultdict(int)
+        self.all_links = set([url])
+        self.max_links = 0
 
     def run(self):
         """
@@ -44,7 +43,7 @@ class Worker(threading.Thread):
             if random.random() < 0.95 and len(set(self.last10)) > 4:
                 try:
                     # list of urls with words in stopwords filtered out
-                    soup, url_list = parse_links(current_url, url_start)
+                    soup, url_list = self.parse_links(current_url, self.url)
                     url_list = filter(lambda link: sum([word in link for word in stopwords]) == 0, url_list)
                     current_url = random.choice(url_list)
                     current_url_title = soup.title.text.strip()[:-14]  # removes MayoClinic
@@ -53,66 +52,61 @@ class Worker(threading.Thread):
 
                     if "/symptoms/" in current_url or "/diseases-conditions/" in current_url:
                         current_symptom_disease = current_url.split("http://www.mayoclinic.org")[1].split("/")[2]
-                        if (pagesdict[current_symptom_disease][1] == 1):
+                        if (self.pagesdict[current_symptom_disease][1] == 1):
                             # Keep track of links only if the current link is a symptom or disease,
                             # and if we haven't encountered this page before. If we haven't encountered
                             # the page before we need to update all relevant symptoms/diseases that are
                             # linked to by this page with this page.
-                            related_symps_diseases = set()
+                            related_symps_diseases = []
                             for a in set(hrefs):
                                 split_link = a["href"].split("/")
                                 if "symptoms" in split_link:
-                                    # Related link is a symptom.
                                     symptom = split_link[2]
-                                    related_symps_diseases.add(symptom)
-                                    # Add the current page to the list of pages that link to the symptom
-                                    pagesdict[symptom][0].add(current_symptom_disease)
-                                    ranked_symptoms[symptom] = 1
+                                    related_symps_diseases.append(symptom)
+                                    # Related link is a symptom.
+                                    self.pagesdict[symptom][0].append(current_symptom_disease)
+                                    self.symptoms[symptom] = 1
                                 elif "diseases-conditions" in split_link:
-                                    # Related link is a disease or condition.
                                     disease = split_link[2]
-                                    related_symps_diseases.add(disease)
-                                    # Add the current page to the list of pages that link to the disease
-                                    pagesdict[disease][0].add(current_symptom_disease)
-                                    ranked_diseases[disease] = 1
+                                    related_symps_diseases.append(disease)
+                                    # Related link is a disease or condition.
+                                    self.pagesdict[disease][0].append(current_symptom_disease)
+                                    self.diseases[disease] = 1
                             # Update the number of symptoms/diseases this page links to
-                            num_links = len(related_symps_diseases)
-                            pagesdict[current_symptom_disease][0].update(related_symps_diseases)
-                            pagesdict[current_symptom_disease][1] = num_links
-                            num_external_links.append(num_links)
-                            # if num_links > max_links:
-                            #     max_links = num_links
+                            num_links = len(set(related_symps_diseases))
+                            self.pagesdict[current_symptom_disease][1] = num_links
+                            if num_links > self.max_links:
+                                self.max_links = num_links
                 except:
-                    current_url = random.sample(all_links, 1)[0]
+                    current_url = random.sample(self.all_links, 1)[0]
             else:
                 # click the "home" button!
-                current_url = random.sample(all_links, 1)[0]
+                current_url = random.sample(self.all_links, 1)[0]
 
             self.last10.popleft()
             self.last10.append(current_url)
-            all_links.add(current_url)
+            self.all_links.add(current_url)
 
-
-def parse_links(url, url_start):
-    url_list = []
-    # open, read, and parse the text using beautiful soup
-    try:
-        page = urllib.urlopen(url)
-        text = page.read()
-        page.close()
-        soup = BeautifulSoup(text, "lxml")
-        # find all hyperlinks using beautiful soup
-        hrefs = soup.find("div", {"class": "contentbox links show-more"}).findAll("a", href=True)
-        for tag in set(hrefs):
-            # concatenate the base url with the path from the hyperlink
-            if tag['href'].startswith('/symptoms/') or tag['href'].startswith('/diseases-conditions/'):
-                tmp = urlparse.urljoin(url, tag['href'])
-                url_list.append(tmp)
-        if len(url_list) == 0:
-            return soup, [url_start]
-        return soup, url_list
-    except:
-        return None, [url_start]
+    def parse_links(self, url, url_start):
+        url_list = []
+        # open, read, and parse the text using beautiful soup
+        try:
+            page = urllib.urlopen(url)
+            text = page.read()
+            page.close()
+            soup = BeautifulSoup(text, "lxml")
+            # find all hyperlinks using beautiful soup
+            hrefs = soup.find("div", {"class": "contentbox links show-more"}).findAll("a", href=True)
+            for tag in set(hrefs):
+                # concatenate the base url with the path from the hyperlink
+                if tag['href'].startswith('/symptoms/') or tag['href'].startswith('/diseases-conditions/'):
+                    tmp = urlparse.urljoin(url, tag['href'])
+                    url_list.append(tmp)
+            if len(url_list) == 0:
+                return soup, [url_start]
+            return soup, url_list
+        except:
+            return None, [url_start]
 
 
 def assign_page_rank(pagesdict, ranked_diseases, ranked_symptoms, max_links):
@@ -147,7 +141,6 @@ def assign_page_rank(pagesdict, ranked_diseases, ranked_symptoms, max_links):
         try:
             # If fewer than 5 disease pages change relative rank, then we can
             # say PageRank is finished.
-            num_over_threshold = 0
             for key in pagesdict:
                 new_rank = 1
                 for link in pagesdict[key][0]:
@@ -156,7 +149,7 @@ def assign_page_rank(pagesdict, ranked_diseases, ranked_symptoms, max_links):
                         new_rank += ranked_diseases[link]*(pagesdict[link][1]/max_links)
                     elif link in ranked_symptoms:
                         # Link was from a symptom.
-                        new_rank += 0.5*ranked_symptoms[link]*(pagesdict[link][1]/max_links)
+                        new_rank += 0.25*ranked_symptoms[link]*(pagesdict[link][1]/max_links)
                 if key in ranked_diseases:
                     # Looking at a disease. Assign the new rank.
                     ranked_diseases[key] = new_rank
@@ -184,21 +177,13 @@ stopwords = ['video', 'media', '#']
 pagesdict = defaultdict(lambda: [set(), 1])
 ranked_diseases = defaultdict(lambda: 1)
 ranked_symptoms = defaultdict(lambda: 1)
-num_external_links = []
-
-# if len(sys.argv) > 2:
-#     # read dictionary from json file
-#     with open(sys.argv[2], 'r') as file:
-#         jsondict = json.load(file)
-#     for key, val in jsondict.items():
-#         pagesdict[key] = val
+max_external_links = 0
 
 url_start1 = "http://www.mayoclinic.org/symptoms/cough/basics/definition/sym-20050846"
 url_start2 = "http://www.mayoclinic.org/symptoms/nausea/basics/definition/sym-20050736"
 url_start3 = "http://www.mayoclinic.org/diseases-conditions/carbon-monoxide/basics/definition/con-20025444"
 url_start4 = "http://www.mayoclinic.org/diseases-conditions/heart-attack/basics/definition/con-20019520"
 start_links = [url_start1, url_start2, url_start3, url_start4]
-all_links = set(start_links)
 workers = []
 t = time.time()
 
@@ -208,6 +193,12 @@ for i, url_start in enumerate(start_links):
     workers.append(worker)
 
 for worker in workers:
+    for key in worker.pagesdict:
+        pagesdict[key.replace('-', ' ')][0].update(worker.pagesdict[key][0])
+        pagesdict[key][1] = max(pagesdict[key][1], worker.pagesdict[key][1])
+    ranked_diseases.update(worker.diseases)
+    ranked_symptoms.update(worker.symptoms)
+    max_external_links = max(max_external_links, worker.max_links)
     worker.join()
 
 for x in pagesdict.items():
@@ -217,7 +208,7 @@ for x in pagesdict.items():
 # with open('symptoms.json', 'w') as file:
 #     file.write(json.dumps(pagesdict))
 
-ranked = assign_page_rank(pagesdict, ranked_diseases, ranked_symptoms, max(num_external_links))
+ranked = assign_page_rank(pagesdict, ranked_diseases, ranked_symptoms, max_external_links)
 page_ranks = [pair[0] for pair in sorted(ranked.items(), key=lambda item: item[1], reverse=True)]
 top_score = float(ranked_symptoms[page_ranks[0]])
 print top_score
